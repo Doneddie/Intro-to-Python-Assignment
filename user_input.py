24b32c261a8e15902da84683f5c47411366c027c
EdwardMase <105016548+Doneddie@users.noreply.github.com>
2024-08-19 12:32:04 +0300

    Answers to my assignment

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+# Asking user for input
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where are you located? ")
+
+# Printing the personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
